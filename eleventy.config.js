123611d8865e2cf2afdbdbc73d9eaa2f964bc8ab
mohamed housni <mhousni@mohameds-MacBook-Pro.local>
2024-02-03 16:10:23 +0100

    patch fixes broken links  MH 1

--- eleventy.config.js
+++ eleventy.config.js
@@ -31,7 +31,7 @@ module.exports = function(eleventyConfig) {
     dir: {
       input: 'src',
       includes: '_includes',
-      output: '_site',
+      output: 'dist',
     },
     templateFormats: ['md', 'njk', 'html'],
     markdownTemplateEngine: 'njk',
